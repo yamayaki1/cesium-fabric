2a8fc33fd1ff409570e9d890299715cf4088b3e6
Yamayaki <65787801+yamayaki1@users.noreply.github.com>
2024-04-15 21:27:27 +0200

    Cleanup IOWorker Mixin

--- src/main/java/de/yamayaki/cesium/mixin/core/storage/MixinIOWorker.java
+++ src/main/java/de/yamayaki/cesium/mixin/core/storage/MixinIOWorker.java
@@ -1,6 +1,5 @@
 package de.yamayaki.cesium.mixin.core.storage;
 
-import com.mojang.datafixers.util.Either;
 import de.yamayaki.cesium.accessor.DatabaseActions;
 import de.yamayaki.cesium.accessor.DatabaseSetter;
 import de.yamayaki.cesium.accessor.SpecificationSetter;
@@ -11,38 +10,22 @@ import net.minecraft.nbt.StreamTagVisitor;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.storage.IOWorker;
 import net.minecraft.world.level.chunk.storage.RegionFileStorage;
-import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
+@Debug(export = true)
 @Mixin(IOWorker.class)
 public abstract class MixinIOWorker implements DatabaseSetter, SpecificationSetter, DatabaseActions {
     @Shadow
     @Final
-    private static Logger LOGGER;
-
-    @Shadow
-    @Final
     private RegionFileStorage storage;
-
-    @Shadow
-    @Final
-    private Map<ChunkPos, IOWorker.PendingStore> pendingWrites;
-
-    @Shadow
-    protected abstract <T> CompletableFuture<T> submitTask(Supplier<Either<T, Exception>> supplier);
 
     @Unique
     private LMDBInstance lmdbStorage;
@@ -55,103 +38,60 @@ public abstract class MixinIOWorker implements DatabaseSetter, SpecificationSett
 
     /**
      * @author Yamayaki
-     * @reason Stupid lambda
+     * @see IOWorker#loadAsync(ChunkPos)
      */
-    @Overwrite
-    public CompletableFuture<Optional<CompoundTag>> loadAsync(ChunkPos chunkPos) {
-        return this.submitTask(() -> {
-            IOWorker.PendingStore pendingStore = this.pendingWrites.get(chunkPos);
-            if (pendingStore != null) {
-                return Either.left(Optional.ofNullable(pendingStore.data));
-            } else {
-                try {
-                    CompoundTag compoundTag;
-                    if (this.isCesium) {
-                        compoundTag = this.lmdbStorage
-                                .getDatabase(this.databaseSpec)
-                                .getValue(chunkPos);
-                    } else {
-                        compoundTag = this.storage.read(chunkPos);
-                    }
-
-                    return Either.left(Optional.ofNullable(compoundTag));
-                } catch (Exception var4) {
-                    LOGGER.warn("Failed to read chunk {}", chunkPos, var4);
-                    return Either.right(var4);
-                }
-            }
-        });
+    @Redirect(method = "method_27943", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/RegionFileStorage;read(Lnet/minecraft/world/level/ChunkPos;)Lnet/minecraft/nbt/CompoundTag;"))
+    private CompoundTag cesium$read(RegionFileStorage instance, ChunkPos chunkPos) throws IOException {
+        if (this.isCesium) {
+            return this.lmdbStorage
+                    .getDatabase(this.databaseSpec)
+                    .getValue(chunkPos);
+        } else {
+            return instance.read(chunkPos);
+        }
     }
 
     /**
      * @author Yamayaki
-     * @reason Stupid lambda
+     * @see IOWorker#scanChunk(ChunkPos, StreamTagVisitor)
      */
-    @Overwrite
-    public CompletableFuture<Void> synchronize(boolean bl) {
-        CompletableFuture<Void> completableFuture = this.submitTask(() -> Either.left(
-                CompletableFuture.allOf(
-                        this.pendingWrites.values().stream()
-                                .map((pendingStore) -> pendingStore.result)
-                                .toArray(CompletableFuture[]::new))
-        )).thenCompose(Function.identity());
-
-        return bl ? completableFuture.thenCompose((void_) -> this.submitTask(() -> {
-            try {
-                if (!this.isCesium) {
-                    this.storage.flush();
-                }
-
-                return Either.left(null);
-            } catch (Exception var2) {
-                LOGGER.warn("Failed to synchronize chunks", var2);
-                return Either.right(var2);
-            }
-        })) : completableFuture.thenCompose((void_) -> this.submitTask(() -> Either.left(null)));
+    @Redirect(method = "method_39801", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/RegionFileStorage;scanChunk(Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/nbt/StreamTagVisitor;)V"))
+    private void cesium$scanChunk(RegionFileStorage instance, ChunkPos chunkPos, StreamTagVisitor streamTagVisitor) throws IOException {
+        if (this.isCesium) {
+            this.lmdbStorage
+                    .getDatabase(this.databaseSpec)
+                    .scan(chunkPos, streamTagVisitor);
+        } else {
+            instance.scanChunk(chunkPos, streamTagVisitor);
+        }
     }
 
     /**
      * @author Yamayaki
-     * @reason Stupid lambda
+     * @see IOWorker#synchronize(boolean)
      */
-    @Overwrite
-    public CompletableFuture<Void> scanChunk(ChunkPos chunkPos, StreamTagVisitor streamTagVisitor) {
-        return this.submitTask(() -> {
-            try {
-                IOWorker.PendingStore pendingStore = this.pendingWrites.get(chunkPos);
-                if (pendingStore != null) {
-                    if (pendingStore.data != null) {
-                        pendingStore.data.acceptAsRoot(streamTagVisitor);
-                    }
-                } else {
-                    if (this.isCesium) {
-                        this.lmdbStorage.getDatabase(this.databaseSpec).scan(chunkPos, streamTagVisitor);
-                    } else {
-                        this.storage.scanChunk(chunkPos, streamTagVisitor);
-                    }
-                }
-
-                return Either.left(null);
-            } catch (Exception var4) {
-                LOGGER.warn("Failed to bulk scan chunk {}", chunkPos, var4);
-                return Either.right(var4);
-            }
-        });
+    @Redirect(method = "method_27946", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/RegionFileStorage;flush()V"))
+    private void cesium$flush(RegionFileStorage instance) throws IOException {
+        if(!this.isCesium) {
+            instance.flush();
+        }
     }
 
     @Redirect(method = "runStore", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/RegionFileStorage;write(Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/nbt/CompoundTag;)V"))
     private void cesium$write(RegionFileStorage instance, ChunkPos chunkPos, CompoundTag compoundTag) throws IOException {
         if (this.isCesium) {
-            this.lmdbStorage.getTransaction(this.databaseSpec).add(chunkPos, compoundTag);
+            this.lmdbStorage
+                    .getTransaction(this.databaseSpec)
+                    .add(chunkPos, compoundTag);
         } else {
-            this.storage.write(chunkPos, compoundTag);
+            instance.write(chunkPos, compoundTag);
         }
     }
 
     @Redirect(method = "close", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/storage/RegionFileStorage;close()V"))
     private void cesium$close(RegionFileStorage instance) throws IOException {
         if (!this.isCesium) {
-            this.storage.close();
+            instance.close();
         }
     }
 
